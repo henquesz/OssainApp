1ed1d9831d2483cde8dbf22845367b58a539289f
henquesz <henquesvinicius@gmail.com>
2022-08-22 14:46:58 -0300

    first commit

---
+++ firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import * as firebase from "firebase";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9HNIGEqUhr-uhrUHBAQYaIvGD_RSThcQ",
+  authDomain: "ossainapp-52cf0.firebaseapp.com",
+  projectId: "ossainapp-52cf0",
+  storageBucket: "ossainapp-52cf0.appspot.com",
+  messagingSenderId: "351470068640",
+  appId: "1:351470068640:web:82b8e91b4fb0683496fac5"
+};
+
+// Initialize Firebase
+let app;
+
+if(firebase.apps.length === 0){
+    app = firebase.initializeApp(firebaseConfig);
+}else{
+    app = firebase.app()
+}
+
+const auth = firebase.auth()
+
+export { auth };
---
+++ screens/LoginScreen.js
@@ -0,0 +1,84 @@
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import React, {useState} from 'react'
+import HomeScreen from './HomeScreen'
+
+const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>{`Hello again.\nWelcome back.`}</Text>
+
+      <View style={styles.errorMessage}>
+        {/* <Text>error</Text> */}
+      </View>
+
+      <View style={styles.form}>
+        <View>
+            <Text style={styles.inputTitle}>Email Adress</Text>
+            <TextInput style={styles.input} autoCapitalize="none" placeholder="Email" value={email} onChangeText={text => setEmail(text)}></TextInput>
+        </View>
+
+        <View style={{paddingTop: 25}}>
+            <Text style={styles.inputTitle}>Password</Text>
+            <TextInput style={styles.input} secureTextEntry autoCapitalize="none" placeholder="Password" value={password} onChangeText={text => setPassword(text)}></TextInput>
+        </View>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={HomeScreen}>
+        <Text style={{color: "white"}}>Sign in</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={{alignSelf: "center", marginTop:32}}>
+        <Text style={{color: "#2d2d2d"}}>
+            New to SocialApp? <Text style={{color: "#5ac1ae", fontWeight: "500"}}>Sign up.</Text>
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    greeting: {
+        marginTop: 100,
+        fontSize: 18,
+        fontWeight: "400",
+        textAlign: "center",
+    },
+    errorMessage: {
+        height: 72,
+        alignItems: "center",
+        justifyContent: "center",
+        marginHorizontal: 30, 
+    },
+    form: {
+        marginBottom:48,
+        marginHorizontal:30,
+    },
+    inputTitle: {
+        color: "#5ac1ae",
+        fontSize: 10,
+        textTransform: "uppercase",
+    },
+    input: {
+        borderBottomColor: "#5ac1ae",
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        height: 40,
+        fontSize: 15,
+        color: "#5ac1ae",
+    },
+    button: {
+        marginHorizontal: 30,
+        backgroundColor: "#5ac1ae",
+        borderRadius: 4,
+        height: 52,
+        alignItems: "center",
+        justifyContent: "center",
+    }
+})
