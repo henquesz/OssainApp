e3b2b7ed2dfe57bea01505c40b92e77a6e4cabf8
henquesz <henquesvinicius@gmail.com>
2022-08-24 14:30:04 -0300

    Validação de login e redirecionamento pra HomeScreen finalizados

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,23 +6,44 @@ import {
   TextInput,
   TouchableOpacity,
 } from "react-native";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HomeScreen from "./HomeScreen";
 import { auth } from "../firebase";
+import { useNavigation } from "@react-navigation/native";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigation = useNavigation()
+
+  useEffect(() => {
+  const unsubscribe = auth.onAuthStateChanged(user => {
+      if(user){
+        navigation.navigate("Home")
+      }
+    })
+    return unsubscribe
+  }, [])
 
   const handleSignUp = () => {
     auth
       .createUserWithEmailAndPassword(email, password)
       .then((userCredentials) => {
         const user = userCredentials.user;
-        console.log(user.email);
+        console.log('Registered in', user.email);
       })
       .catch((error) => alert(error.message));
-  };
+  }
+  
+  const handleSign = () => {
+    auth
+    .signInWithEmailAndPassword(email, password)
+    .then(userCredentials => {
+      const user = userCredentials.user;
+      console.log('Logged with', user.email);
+    }).catch(error => alert(error.message))
+  }
 
   return (
     <KeyboardAvoidingView style={styles.container}>
@@ -56,7 +77,7 @@ const LoginScreen = () => {
           </View>
         </View>
 
-        <TouchableOpacity style={styles.button} onPress={HomeScreen}>
+        <TouchableOpacity style={styles.button} onPress={handleSign}>
           <Text style={{ color: "white" }}>Sign in</Text>
         </TouchableOpacity>
 
