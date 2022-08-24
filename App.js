9ea4a5db1b57db638c0c1ea0dfc185372b4a750a
henquesz <henquesvinicius@gmail.com>
2022-08-24 14:34:32 -0300

    HomeScreen header disable

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen options={{headerShown: false}} name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen options={{headerShown: false}} name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
